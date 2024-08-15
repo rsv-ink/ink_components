11be1a7fa5a06008de42c9f70626c8ba2db9328f
natannobre <natannobre37@gmail.com>
2024-08-15 10:27:26 -0300

    config: configura resource example
    Co-authored-by: Karine Vieira <62672837+karinevieira@users.noreply.github.com>
    Co-authored-by: Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>

---
+++ app/components/ink_components/example_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+module InkComponents
+  class ExampleComponent < ViewComponent::Base
+  end
+end
---
+++ app/helpers/ink_components/examples_helper.rb
@@ -0,0 +1,4 @@
+module InkComponents
+  module ExamplesHelper
+  end
+end
