47edec704e34652176bcada8355ab5eee76ca56c
natannobre <natannobre37@gmail.com>
2024-08-15 12:15:24 -0300

    WIP: configura incialmente o flowbite, mas o comportamento não está correto.
    Co-authored-by: Karine Vieira <62672837+karinevieira@users.noreply.github.com>
    Co-authored-by: Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>

--- spec/dummy/app/javascript/application.js
+++ spec/dummy/app/javascript/application.js
@@ -1,3 +1,6 @@
+import 'flowbite'
+
+// This code below is for reference only
 import { foo } from 'ink_components'
 
 foo()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  plugins: [require("flowbite/plugin")],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
     './app/components/ink_components/**/*.html.erb',
-    './app/components/ink_components/**/*.rb'
+    './app/components/ink_components/**/*.rb',
+    "./node_modules/flowbite/**/*.js"
   ]
 }
