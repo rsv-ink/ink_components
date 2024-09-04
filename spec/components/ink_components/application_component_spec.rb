3e83f435d1816f31cfe5a649b478e7332aef3f89
David Amurim <davidamurim7@gmail.com>
2024-09-04 16:38:44 -0300

    refactor: ajuste de codigo

--- spec/components/ink_components/application_component_spec.rb
+++ spec/components/ink_components/application_component_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
     context "when merging attributes" do
       it "add extra attributes to the component" do
         extra_attributes = { data: { controller: "example" } }
-        
+
         component = described_class.new(**extra_attributes)
 
         expect(component.attributes[:data]).to eq(controller: "example")
@@ -16,9 +16,9 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
       it "concatenates class attributes" do
         default_attributes = { class: "bg-blue-500" }
         extra_attributes = { class: "text-white" }
-        
+
         allow_any_instance_of(described_class).to receive(:default_attributes).and_return(default_attributes)
-       
+
         component = described_class.new(**extra_attributes)
 
         expect(component.attributes[:class]).to include("bg-blue-500 text-white")
@@ -29,9 +29,9 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
       it "merges Tailwind classes without conflits" do
         default_attributes = { class: "bg-blue-500 text-blue" }
         extra_attributes = { class: "bg-black-500 text-white" }
-        
+
         allow_any_instance_of(described_class).to receive(:default_attributes).and_return(default_attributes)
-        
+
         component = described_class.new(**extra_attributes)
 
         expect(component.attributes[:class]).to eq("bg-black-500 text-white")
