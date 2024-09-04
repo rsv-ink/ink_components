e998081af0a1ac249331745cbff459c765f96368
David Amurim <davidamurim7@gmail.com>
2024-09-04 15:06:01 -0300

    refactor: ajusta testes unitarios

--- spec/components/ink_components/application_component_spec.rb
+++ spec/components/ink_components/application_component_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
     context "when merging attributes" do
       it "merges default and user attributes correctly" do
         default_attributes = { class: "bg-blue-500", id: "default-id" }
+        allow_any_instance_of(described_class).to receive(:default_attributes).and_return(default_attributes)
         user_attributes = { class: "text-white", id: "user-id", data: { controller: "example" } }
-        component = described_class.new(default_attributes: default_attributes, **user_attributes)
+        component = described_class.new(**user_attributes)
 
         expect(component.attributes[:id]).to eq("default-id user-id")
         expect(component.attributes[:data]).to eq(controller: "example")
@@ -16,8 +17,9 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
 
       it "concatenates class attributes" do
         default_attributes = { class: "bg-blue-500", id: "default-id" }
+        allow_any_instance_of(described_class).to receive(:default_attributes).and_return(default_attributes)
         user_attributes = { class: "text-white", id: "user-id", data: { controller: "example" } }
-        component = described_class.new(default_attributes: default_attributes, **user_attributes)
+        component = described_class.new(**user_attributes)
 
         expect(component.attributes[:class]).to include("bg-blue-500")
         expect(component.attributes[:class]).to include("text-white")
@@ -27,8 +29,9 @@ RSpec.describe InkComponents::ApplicationComponent, type: :component do
     context "when processing Tailwind classes" do
       it "merges Tailwind classes correctly" do
         default_attributes = { class: "bg-blue-500 bg-red-500 mb-1 text-blue" }
+        allow_any_instance_of(described_class).to receive(:default_attributes).and_return(default_attributes)
         user_attributes = { class: "bg-black-500 text-white" }
-        component = described_class.new(default_attributes: default_attributes, **user_attributes)
+        component = described_class.new(**user_attributes)
 
         expect(component.attributes[:class]).to eq("mb-1 bg-black-500 text-white")
       end
