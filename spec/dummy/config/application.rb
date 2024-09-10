cd9228974b497a48f9c1c8def6bb76e3ac6eed0d
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-09-10 10:05:54 -0300

    config: Configura CI para executar os testes (#44)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -29,6 +29,7 @@ module Dummy
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths += %W[#{config.root}/lib/generators]
 
     # Configuration for the application, engines, and railties goes here.
     #
