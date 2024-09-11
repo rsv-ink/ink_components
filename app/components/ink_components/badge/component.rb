96e010b029bbc967ce42e6bb0a42dd341fd038ca
David Amurim <davidamurim7@gmail.com>
2024-09-11 17:31:24 -0300

    feat: Adiciona parametros do badge

--- app/components/ink_components/badge/component.rb
+++ app/components/ink_components/badge/component.rb
@@ -4,7 +4,7 @@ module InkComponents
   module Badge
     class Component < ApplicationComponent
       style do
-        base { "font-medium me-2 px-2.5 py-0.5" }
+        base { "font-medium me-2" }
 
         variants {
           color {
@@ -18,49 +18,95 @@ module InkComponents
             purple { "bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300" }
           }
 
-          bordered {
-            yes { "dark:bg-gray-700 border" }
-          }
-
           size {
             xs { "text-xs" }
             sm { "text-sm" }
+          }
+
+          bordered {
+            yes do |color:, **|
+              %w[dark:bg-gray-700 border].tap do
+                _1 << "border-pink-400 dark:text-pink-400" if color == :pink
+                _1 << "border-gray-500 dark:text-gray-400" if color == :dark
+                _1 << "border-blue-400 dark:text-blue-400" if color == :blue
+                _1 << "border-red-400 dark:text-red-400" if color == :red
+                _1 << "border-green-400 dark:text-green-400" if color == :green
+                _1 << "border-yellow-300 dark:text-yellow-300" if color == :yellow
+                _1 << "border-indigo-400 dark:text-indigo-400" if color == :indigo
+                _1 << "border-purple-400 dark:text-purple-400" if color == :purple
+              end
+            end
           }
 
           pill {
             yes { "rounded-full" }
             no { "rounded" }
           }
+
+          href {
+            yes do |color:, **|
+              %w[inline-flex items-center justify-center].tap do
+                _1 << "hover:bg-pink-200" if color == :pink
+                _1 << "hover:bg-gray-200" if color == :dark
+                _1 << "hover:bg-blue-200" if color == :blue
+                _1 << "hover:bg-red-200" if color == :red
+                _1 << "hover:bg-green-200" if color == :green
+                _1 << "hover:bg-yellow-200" if color == :yellow
+                _1 << "hover:bg-indigo-200" if color == :indigo
+                _1 << "hover:bg-purple-200" if color == :purple
+              end
+            end
+          }
+
+          dismissable {
+            yes { "inline-flex items-center px-2 py-1 me-2" }
+            no { "px-2.5 py-0.5" }
+          }
         }
 
-        # compound_variants [
-        #   { color: :pink, bordered: :yes, classes: "border-pink-400 dark:text-pink-400" },
-        #   { color: :dark, bordered: :yes, classes: "border-gray-500 dark:text-gray-400" },
-        #   { color: :blue, bordered: :yes, classes: "border-blue-400 dark:text-blue-400" },
-        #   { color: :red, bordered: :yes, classes: "border-red-400 dark:text-red-400" },
-        #   { color: :green, bordered: :yes, classes: "border-green-400 dark:text-green-400" },
-        #   { color: :yellow, bordered: :yes, classes: "border-yellow-300 dark:text-yellow-300" },
-        #   { color: :indigo, bordered: :yes, classes: "border-indigo-400 dark:text-indigo-400" },
-        #   { color: :purple, bordered: :yes, classes: "border-purple-400 dark:text-purple-400" }
-        # ]
-
-        defaults { { color: :pink, bordered: :no, size: :xs, pill: :no } }
+        defaults { { color: :pink, size: :xs, bordered: :no, pill: :no, href: :no, dismissable: :no } }
       end
 
-      attr_reader :color, :bordered, :size, :pill
+      style :dismiss_button do
+        base { "inline-flex items-center p-1 ms-2 text-sm bg-transparent rounded-sm" }
 
-      def initialize(color: nil, bordered: nil, size: nil, pill: nil, **extra_attributes)
+        variants {
+          color {
+            pink { "text-pink-400 hover:bg-pink-200 hover:text-pink-900 dark:hover:bg-pink-800 dark:hover:text-pink-300" }
+            dark { "text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-300" }
+            blue { "text-blue-400 hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300" }
+            red { "text-red-400 hover:bg-red-200 hover:text-red-900 dark:hover:bg-red-800 dark:hover:text-red-300" }
+            green { "text-green-400 hover:bg-green-200 hover:text-green-900 dark:hover:bg-green-800 dark:hover:text-green-300" }
+            yellow { "text-yellow-400 hover:bg-yellow-200 hover:text-yellow-900 dark:hover:bg-yellow-800 dark:hover:text-yellow-300" }
+            indigo { "text-indigo-400 hover:bg-indigo-200 hover:text-indigo-900 dark:hover:bg-indigo-800 dark:hover:text-indigo-300" }
+            purple { "text-purple-400 hover:bg-purple-200 hover:text-purple-900 dark:hover:bg-purple-800 dark:hover:text-purple-300" }
+          }
+        }
+
+        defaults { { color: :pink } }
+      end
+
+      attr_reader :id, :color, :size, :bordered, :pill, :href, :dismissable
+
+      def initialize(id: nil, color: nil, size: nil, bordered: nil, pill: nil, href: nil, dismissable: nil, **extra_attributes)
+        @id = id
         @color = color
-        @bordered = bordered
         @size = size
+        @bordered = bordered
         @pill = pill
+        @href = href
+        @dismissable = dismissable
 
         super(**extra_attributes)
       end
 
       private
       def default_attributes
-        { class: style(color:, bordered:, size:, pill:) }
+        { id:, class: style(color:, bordered:, size:, pill:, href: href.present?, dismissable:), href: }
+      end
+
+      def dismiss_button_attributes
+        { class: style(:dismiss_button, color:), type: "button", "aria-label": "Remove", "data-dismiss-target": "##{id}" }
       end
 
       def render?
