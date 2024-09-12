c10157280a50107e33ae93b531199b9e310b8aae
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-09-12 15:05:21 -0300

    feat: Cria componente Label (#50)

---
+++ app/components/ink_components/forms/label/preview.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module InkComponents
+  module Forms
+    module Label
+      class Preview < Lookbook::Preview
+        # @param content text
+        # @param state select { choices: [default, success, error] }
+        def default(content: "Your name", state: :default)
+          render InkComponents::Forms::Label::Component.new(state:).with_content(content)
+        end
+
+        def with_html_attributes
+          render InkComponents::Forms::Label::Component.new(id: "name").with_content("Your name")
+        end
+      end
+    end
+  end
+end
