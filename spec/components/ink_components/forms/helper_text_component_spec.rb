6806be6832d1610bf78e0ee6293b2b9da2531968
natannobre <natannobre37@gmail.com>
2024-09-12 14:13:01 -0300

    Revert "feat: adiciona testes para variantes do componente."
    This reverts commit 494ac5c227b06be0044650733be3b9d4f5a1f3ae.

--- spec/components/ink_components/forms/helper_text_component_spec.rb
+++ spec/components/ink_components/forms/helper_text_component_spec.rb
@@ -18,44 +18,4 @@ RSpec.describe InkComponents::Forms::HelperText::Component, type: :component do
       expect(component.to_html).to be_empty
     end
   end
-
-  context "when size is not set" do
-    it "applies default class" do
-      component = render_inline(described_class.new) { "Some text" }
-
-      expect(component.css("p").first["class"]).to include("text-sm")
-    end
-  end
-
-  context "when size is set to :xs" do
-    it "applies the correct class" do
-      component = render_inline(described_class.new(size: :xs)) { "Some text" }
-
-      expect(component.css("p").first["class"]).to include("text-xs")
-    end
-  end
-
-  context "when state is not set" do
-    it "applies default class" do
-      component = render_inline(described_class.new) { "Some text" }
-
-      expect(component.css("p").first["class"]).to include("text-gray-500")
-    end
-  end
-
-  context "when state is set to :error" do
-    it "applies the correct class" do
-      component = render_inline(described_class.new(state: :error)) { "Some text" }
-
-      expect(component.css("p").first["class"]).to include("text-red-600")
-    end
-  end
-
-  context "when state is set to :success" do
-    it "applies the correct class" do
-      component = render_inline(described_class.new(state: :success)) { "Some text" }
-
-      expect(component.css("p").first["class"]).to include("text-green-600")
-    end
-  end
 end
