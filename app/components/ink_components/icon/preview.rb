e14280bf555a8c81efb47d9e063e25d3de6f7445
Lucas Sousa <lucas.oliveira@reserva.ink>
2024-09-26 13:36:41 -0300

    feat: Adiciona os ícones do Flowbite

--- app/components/ink_components/icon/preview.rb
+++ app/components/ink_components/icon/preview.rb
@@ -2,14 +2,30 @@ module InkComponents
   module Icon
     class Preview < Lookbook::Preview
       # @!group Types
-      def with_solid_type
-        render InkComponents::Icon::Component.new(name: :bell, class: "w-6 h-6 fill-gray-500")
+      # @param name select :icon_names "The icon name"
+      def with_solid_type(name: :bell)
+        render InkComponents::Icon::Component.new(name:, class: "w-6 h-6 fill-gray-500")
       end
 
-      def with_outline_type
-        render InkComponents::Icon::Component.new(name: :bell, type: :outline, class: "w-6 h-6 text-gray-500")
+      # @param name select :icon_names "The icon name"
+      def with_outline_type(name: :bell)
+        render InkComponents::Icon::Component.new(name:, type: :outline, class: "w-6 h-6 text-gray-500")
       end
       # @!endgroup
+
+      private
+      def icon_names
+        solid = list_icon_names("#{InkComponents::Engine.root}/app/assets/images/ink_components/icons/solid/*.svg")
+        outline = list_icon_names("#{InkComponents::Engine.root}/app/assets/images/ink_components/icons/outline/*.svg")
+
+        solid + outline
+      end
+
+      def list_icon_names(path)
+        Dir.glob(path).map do |file|
+          File.basename(file, ".svg")
+        end
+      end
     end
   end
 end
