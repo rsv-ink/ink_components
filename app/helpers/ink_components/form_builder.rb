21be620baf87efca4344a07162cde0037e0a2951
natannobre <natannobre37@gmail.com>
2024-10-16 16:56:11 -0300

    feat: implementa label no form builder

--- app/helpers/ink_components/form_builder.rb
+++ app/helpers/ink_components/form_builder.rb
@@ -2,29 +2,22 @@
 
 module InkComponents
   class FormBuilder < ActionView::Helpers::FormBuilder
-    def label(attribute, content, **)
-      label_component(for: id(attribute), **) { content }
+    include InkComponents::ViewHelper
+
+    attr_reader :template
+
+    delegate :render, to: :template
+
+    def label(attribute, content = nil, **)
+      content = attribute.to_s.humanize if content.nil?
+
+      label_component(for: format_id(attribute), **) { content }
     end
 
-    # def label_options(attribute)
-    #   # html_options(attribute).transform_keys({ id: :for })
-    #   { for: id(attribute) }
-    # end
+    def format_id(attribute)
+      resource_name = object_name.present? ? object_name+"_" : ""
 
-    # def html_options(attribute)
-    #   {
-    #     name: name(attribute),
-    #     id: id(attribute),
-    #     value: object.try(:public_send, attribute)
-    #   }
-    # end
-
-    # def name(attribute)
-    #   "#{object_name}[#{attribute}]"
-    # end
-
-    def id(attribute)
-      "#{object_name}_#{attribute}".delete("]").tr("^-a-zA-Z0-9:.", "_")
+      "#{resource_name}#{attribute}".delete("]").tr("^-a-zA-Z0-9:.", "_")
     end
   end
 end
