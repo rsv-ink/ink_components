e6609ec128a6fc03ecb26f1a8b4c9dcdb2753ee8
Natan Nobre Chaves <natannobre37@gmail.com>
2024-10-22 15:34:40 -0300

    feat: Implementa FormBuilder do DS (#84)
    Co-authored-by: alannascimento1 <nascimentoalankst@gmail.com>
    Co-authored-by: Karine Vieira <kv.vieira1@gmail.com>

---
+++ app/helpers/ink_components/form_builder.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module InkComponents
+  class FormBuilder < ActionView::Helpers::FormBuilder
+    include InkComponents::ViewHelper
+
+    attr_reader :template
+
+    delegate :render, to: :template
+
+    def label(attribute, content = nil, **)
+      content ||= label_text(attribute)
+
+      label_component(for: format_id(attribute), **) { content }
+    end
+
+    def check_box(attribute, options = {}, checked_value = "1", unchecked_value = "0")
+      checked = object.try(:public_send, attribute).in?([ true, checked_value ])
+
+      checkbox_component(
+        id: format_id(attribute), name: format_name(attribute), checked_value:, unchecked_value:, checked:, **options
+      )
+    end
+
+    def text_field(attribute, **)
+      state = field_state(attribute)
+      input_field_component(type: :text, state:, **html_options(attribute), **)
+    end
+
+    def error_message(attribute, **)
+      state = field_state(attribute)
+      helper_text_component(state:, **) { error_messages(attribute) }
+    end
+
+    private
+    def label_text(attribute)
+      content ||= if object_name.present?
+        ActionView::Helpers::Tags::Translator.new(object, object_name, attribute, scope: "helpers.label").translate
+      else
+        I18n.t("helpers.label.#{attribute}", default: attribute.to_s.humanize)
+      end
+    end
+
+    def field_state(attribute)
+      return :default if object.nil?
+
+      object.errors.include?(attribute) ? :error : :default
+    end
+
+    def error_messages(attribute)
+      return if object.nil?
+
+      object.errors[attribute].to_sentence.capitalize.presence&.concat(".")
+    end
+
+    def html_options(attribute)
+      {
+        name: format_name(attribute),
+        id: format_id(attribute),
+        value: object.try(:public_send, attribute)
+      }
+    end
+
+    def format_id(attribute)
+      resource_name = object_name.present? ? object_name+"_" : ""
+
+      "#{resource_name}#{attribute}".delete("]").tr("^-a-zA-Z0-9:.", "_")
+    end
+
+    def format_name(attribute)
+      object_name.present? ? "#{object_name}[#{attribute}]" : attribute
+    end
+  end
+end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -41,5 +41,8 @@ module Dummy
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}")]
+    config.i18n.default_locale = :pt
   end
 end
