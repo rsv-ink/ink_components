c9078677f3868247c894d0fa2995aecec883427a
alannascimento1 <nascimentoalankst@gmail.com>
2024-10-31 10:24:21 -0300

    refactor: Remove códigos de teste

--- spec/dummy/app/controllers/admin/groups_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::GroupsController < ApplicationController
-  def new
-    @group = Admin::Group.new([
-      User.new(name: "Alice", email: "alice@example.com"),
-      User.new(name: "Bob", email: "bob@example.com"),
-      User.new(name: "Dani", email: "dani@example.com")
-    ])
-  end
-end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   mount InkComponents::Engine => "/ink_components"
 
   resources :users, only: [ :new, :create ]
-  resources :weeks, only: [ :new, :create ]
-  namespace :admin do
-    resources :groups, only: [ :new, :create ]
-  end
 end
