f535064c7585e1d1fccf68adc4b44b157d28b161
alannascimento1 <nascimentoalankst@gmail.com>
2024-11-01 14:59:30 -0300

    feat: Importa libs inline e tailwind merge

--- lib/ink_components.rb
+++ lib/ink_components.rb
@@ -1,6 +1,8 @@
 require "ink_components/version"
 require "ink_components/engine"
 require "lookbook"
+require "inline_svg"
+require "tailwind_merge"
 
 module InkComponents
   # Your code goes here...
