120410130d7c8df767d193a6b7e1a9c386df7357
alannascimento1 <nascimentoalankst@gmail.com>
2024-11-01 15:42:40 -0300

    fix: remove typo css

--- lib/ink_components.rb
+++ lib/ink_components.rb
@@ -2,7 +2,7 @@ require "ink_components/version"
 require "ink_components/engine"
 require "lookbook"
 require "inline_svg"
-require "tailwindcss/merge"
+require "tailwind_merge"
 require "view_component-contrib"
 
 module InkComponents
