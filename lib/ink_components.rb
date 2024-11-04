f5e6c6034080609c65b2a8cba9c9a3a1be5f7161
Alan Nascimento <nascimentoalankst@gmail.com>
2024-11-04 10:38:12 -0300

    feat: Adiciona importação de libs `inline_svg`, `tailwind_merge` e `view_component-contrib` (#111)
    * feat: Adiciona importação de libs
    
    * fix: remove typo css

--- lib/ink_components.rb
+++ lib/ink_components.rb
@@ -1,6 +1,9 @@
 require "ink_components/version"
 require "ink_components/engine"
 require "lookbook"
+require "inline_svg"
+require "tailwind_merge"
+require "view_component-contrib"
 
 module InkComponents
   # Your code goes here...
