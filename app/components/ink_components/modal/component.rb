b8a1fecafd304afed4875e6c470e0ddfae567160
hoffmannpaola <hoffmannpaola@gmail.com>
2024-12-19 14:35:36 -0300

    feat: adiciona footer e id para modal no preview

--- app/components/ink_components/modal/component.rb
+++ app/components/ink_components/modal/component.rb
@@ -3,6 +3,7 @@ module InkComponents
     class Component < ApplicationComponent
       renders_one :header, "HeaderComponent"
       renders_one :body, "BodyComponent"
+      renders_one :footer, "FooterComponent"
 
 
       style do
@@ -16,15 +17,15 @@ module InkComponents
         }
       end
 
-      attr_reader :size
-
-      def initialize(size: nil)
+      attr_reader :id, :size
+      def initialize(id:, size: nil)
+        @id = id
         @size = size
       end
 
       class HeaderComponent < ApplicationComponent
         def call
-          content_tag :div, class: "flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600" do
+          content_tag :div, class: "w-full flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600" do
             safe_concat(content_tag(:h3, content, class: "text-xl font-semibold text-gray-900 dark:text-white"))
 
             safe_concat(content_tag(:button, type: "button", class: "text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white", data: { modal_hide: "default-modal" }) do
@@ -44,6 +45,18 @@ module InkComponents
       class BodyComponent < ApplicationComponent
         def call
           content_tag :div, class: "w-full p-4 md:p-5 space-y-4" do
+            content
+          end
+        end
+
+        def render?
+          content.present?
+        end
+      end
+
+      class FooterComponent < ApplicationComponent
+        def call
+          content_tag :div, class: "w-full flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600" do
             content
           end
         end
--- app/components/ink_components/modal/preview.rb
+++ app/components/ink_components/modal/preview.rb
@@ -6,10 +6,12 @@ module InkComponents
       # @param title text
       # @param size select { choices: [sm, md, lg, xl] }
       # @param body text
-      def playground(title: "Título do modal", size: :md, body: "Conteúdo do modal")
-        modal_component(size:) do |component|
+      # @param footer text
+      def playground(id: "default-modal", title: "Título do modal", size: :md, body: "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.", footer: "Rodapé")
+        modal_component(id:, size:) do |component|
           component.with_header { title }
           component.with_body { body }
+          component.with_footer { footer }
         end
       end
     end
