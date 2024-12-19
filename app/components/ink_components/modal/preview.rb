39da68fb1e0c6ee4a7a479102c7ba3d4ae789e30
hoffmannpaola <hoffmannpaola@gmail.com>
2024-12-19 14:44:40 -0300

    feat: adiciona variavel global pra texto usado no body do modal

--- app/components/ink_components/modal/preview.rb
+++ app/components/ink_components/modal/preview.rb
@@ -2,12 +2,13 @@
 
 module InkComponents
   module Modal
+    TEXT_BODY = "Nossas camisetas são de excelência em algodão brasileiro, ideais para todos os climas. Todas as cores são 100% algodão; exceto cinzas: 88% algodão e 12% poliéster.".freeze
     class Preview < Lookbook::Preview
       # @param title text
       # @param size select { choices: [sm, md, lg, xl] }
       # @param body text
       # @param footer text
-      def playground(id: "default-modal", title: "Título do modal", size: :md, body: "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.", footer: "Rodapé")
+      def playground(id: "default-modal", title: "Título do modal", size: :md, body: TEXT_BODY, footer: "Rodapé")
         modal_component(id:, size:) do |component|
           component.with_header { title }
           component.with_body { body }
@@ -19,7 +20,7 @@ module InkComponents
       def small
         modal_component(id: "small-modal", size: :sm) do |component|
           component.with_header { "Small" }
-          component.with_body { "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply." }
+          component.with_body { TEXT_BODY }
           component.with_footer { "Rodapé" }
         end
       end
@@ -27,7 +28,7 @@ module InkComponents
       def medium
         modal_component(id: "medium-modal", size: :md) do |component|
           component.with_header { "Medium" }
-          component.with_body { "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply." }
+          component.with_body { TEXT_BODY }
           component.with_footer { "Rodapé" }
         end
       end
@@ -35,7 +36,7 @@ module InkComponents
       def large
         modal_component(id: "large-modal", size: :lg) do |component|
           component.with_header { "Large" }
-          component.with_body { "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply." }
+          component.with_body { TEXT_BODY }
           component.with_footer { "Rodapé" }
         end
       end
@@ -43,7 +44,7 @@ module InkComponents
       def extra_large
         modal_component(id: "extra-large-modal", size: :xl) do |component|
           component.with_header { "Extra Large" }
-          component.with_body { "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply." }
+          component.with_body { TEXT_BODY }
           component.with_footer { "Rodapé" }
         end
       end
