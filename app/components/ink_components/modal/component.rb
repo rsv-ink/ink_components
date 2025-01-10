ce78a3cc82c5f829c993e86d438a559e9d17d9ad
hoffmannpaola <hoffmannpaola@gmail.com>
2025-01-10 14:54:54 -0300

    refactor: adiciona method para retornar icone e com ele deixar código mais legível

--- app/components/ink_components/modal/component.rb
+++ app/components/ink_components/modal/component.rb
@@ -60,12 +60,16 @@ module InkComponents
 
             safe_concat(content_tag(:button, type: "button", class: "text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center",
                                     data: { modal_hide: modal_id }) do
-              safe_concat(raw('<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>'))
+              safe_concat(close_icon)
               safe_concat(content_tag(:span, "Close modal", class: "sr-only"))
             end)
           end
+        end
+
+        def close_icon
+          raw('<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>')
         end
       end
 
