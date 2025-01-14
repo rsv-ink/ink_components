6d074a732ef3851d5453f1eab8a3a91852324bfc
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-01-14 12:59:59 -0300

    fix: Corrige os previews do componente modal (#132)

--- app/components/ink_components/modal/preview.rb
+++ app/components/ink_components/modal/preview.rb
@@ -28,8 +28,8 @@ module InkComponents
           component.with_header(modal_id: "default-modal", title: "Default")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-start items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "default-modal" })
+            component.content_tag :div, class: "w-full flex justify-start items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "default-modal" })
             end
           end
         end
@@ -41,8 +41,8 @@ module InkComponents
           component.with_header(modal_id: "static-modal", title: "Static")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-start items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
+            component.content_tag :div, class: "w-full flex justify-start items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
             end
           end
         end
@@ -54,9 +54,9 @@ module InkComponents
           component.with_header(modal_id: "popup-modal", title: "Popup")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-center items-center p-4 gap-2" do
-              content_tag(:button, "Aceitar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-pink-300 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "popup-modal" }) +
-              content_tag(:button, "Declinar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-gray-600 hover:bg-gray-800 focus:ring-gray-300 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "popup-modal" })
+            component.content_tag :div, class: "w-full flex justify-center items-center p-4 gap-2" do
+              component.content_tag(:button, "Aceitar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-pink-300 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "popup-modal" }) +
+              component.content_tag(:button, "Declinar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-gray-600 hover:bg-gray-800 focus:ring-gray-300 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "popup-modal" })
             end
           end
         end
@@ -70,8 +70,8 @@ module InkComponents
           component.with_header(modal_id: "small-modal", title: "Small")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-end items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
+            component.content_tag :div, class: "w-full flex justify-end items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
             end
           end
         end
@@ -83,8 +83,8 @@ module InkComponents
           component.with_header(modal_id: "medium-modal", title: "Default")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-end items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
+            component.content_tag :div, class: "w-full flex justify-end items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
             end
           end
         end
@@ -96,8 +96,8 @@ module InkComponents
           component.with_header(modal_id: "large-modal", title: "Large")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-end items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
+            component.content_tag :div, class: "w-full flex justify-end items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
             end
           end
         end
@@ -109,8 +109,8 @@ module InkComponents
           component.with_header(modal_id: "extra-large-modal", title: "Extra large")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-end items-center p-4" do
-              content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
+            component.content_tag :div, class: "w-full flex justify-end items-center p-4" do
+              component.content_tag(:button, "Fechar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "static-modal" })
             end
           end
         end
@@ -165,15 +165,15 @@ module InkComponents
           component.with_trigger { button("body-modal") }
           component.with_header(modal_id: "body-modal", title: "Modal com Body Personalizado")
           component.with_body do
-            content_tag :div, class: "w-full" do
-              content_tag :div, class: "w-full grid grid-cols-2 gap-2" do
-                content_tag(:div, class: "flex flex-col gap-2") do
-                  content_tag(:label, "Nome", class: "text-sm font-medium text-gray-900 dark:text-white") +
-                  content_tag(:input, nil, type: "text", class: "border-gray-300 rounded-lg px-3 py-2.5 text-sm focus:ring-4 focus:outline-none", placeholder: "João das Neves")
+            component.content_tag :div, class: "w-full" do
+              component.content_tag :div, class: "w-full grid grid-cols-2 gap-2" do
+                component.content_tag(:div, class: "flex flex-col gap-2") do
+                  component.content_tag(:label, "Nome", class: "text-sm font-medium text-gray-900 dark:text-white") +
+                  component.content_tag(:input, nil, type: "text", class: "border-gray-300 rounded-lg px-3 py-2.5 text-sm focus:ring-4 focus:outline-none", placeholder: "João das Neves")
                 end +
-                content_tag(:div, class: "flex flex-col gap-2") do
-                  content_tag(:label, "Idade", class: "text-sm font-medium text-gray-900 dark:text-white") +
-                  content_tag(:input, nil, type: "text", class: "border-gray-300 rounded-lg px-3 py-2.5 text-sm focus:ring-4 focus:outline-none", placeholder: "34")
+                component.content_tag(:div, class: "flex flex-col gap-2") do
+                  component.content_tag(:label, "Idade", class: "text-sm font-medium text-gray-900 dark:text-white") +
+                  component.content_tag(:input, nil, type: "text", class: "border-gray-300 rounded-lg px-3 py-2.5 text-sm focus:ring-4 focus:outline-none", placeholder: "34")
                 end
               end
             end
@@ -195,8 +195,8 @@ module InkComponents
           component.with_header(modal_id: "footer-modal", title: "Modal com Footer Personalizado")
           component.with_body { TEXT_BODY }
           component.with_footer do
-            content_tag :div, class: "w-full flex justify-end items-center p-4" do
-              content_tag(:button, "Salvar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "footer-modal" })
+            component.content_tag :div, class: "w-full flex justify-end items-center p-4" do
+              component.content_tag(:button, "Salvar", type: "button", class: "focus:ring-4 font-medium text-center focus:outline-none text-white bg-pink-600 hover:bg-pink-800 focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800 rounded-lg px-5 py-2.5 text-sm", data: { modal_hide: "footer-modal" })
             end
           end
         end
