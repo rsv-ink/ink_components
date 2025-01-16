9c8aed593aa4a496679f0f26763204bb0ec4e3f9
hoffmannpaola <hoffmannpaola@gmail.com>
2025-01-16 13:44:05 -0300

    feat: refatora o componente modal para melhorar a estrutura e a legibilidade do código

--- app/components/ink_components/modal/component.rb
+++ app/components/ink_components/modal/component.rb
@@ -6,15 +6,15 @@ module InkComponents
       renders_one :body, InkComponents::Modal::Body::Component
       renders_one :footer, InkComponents::Modal::Footer::Component
 
-      style do
-        variants {
-          max_width {
-            sm { "max-w-md" }
-            md { "max-w-lg" }
-            lg { "max-w-4xl" }
-            xl { "max-w-7xl" }
-          }
+      style :wrapper do
+        base do
+          %w[
+            hidden overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-50
+            w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-900/50
+          ]
+        end
 
+        variants {
           type {
             dynamic { "dynamic" }
             static { "static" }
@@ -38,23 +38,58 @@ module InkComponents
         }
       end
 
-      attr_reader :modal_id, :max_width, :type, :placement, :responsive_sizes
+      style :modal do
+        base do
+          %w[
+            relative max-h-full
+          ]
+        end
 
-      def initialize(modal_id:, max_width: nil, type: :dynamic, placement: :center, responsive_sizes: nil)
+        variants {
+          max_width {
+            sm { "max-w-md" }
+            md { "max-w-lg" }
+            lg { "max-w-4xl" }
+            xl { "max-w-7xl" }
+          }
+
+          padding {
+            yes { "p-4" }
+            no {  }
+          }
+        }
+      end
+
+      attr_reader :modal_id, :max_width, :type, :placement
+
+      def initialize(modal_id:, max_width: nil, type: :dynamic, placement: :center, **extra_attributes)
         @modal_id = modal_id
         @max_width = max_width
         @type = type
         @placement = placement
-        @responsive_sizes = responsive_sizes
+
+        super(**extra_attributes)
       end
 
       private
-      def size
-        max_width.present? ? style(max_width:) : responsive_sizes
+      def wrapper_attributes
+        {
+          id: modal_id,
+          tabindex: -1,
+          data: {
+            modal_backdrop: style(type:),
+            modal_placement: style(placement:)
+          },
+          aria: {
+            hidden: "hidden"
+          },
+          role: "dialog",
+          class: style(:wrapper, placement_classes: placement)
+        }
       end
 
-      def padding
-     "p-4" if max_width.blank?
+      def default_attributes
+        { class: style(:modal, max_width:, padding: max_width.blank?) }
       end
     end
   end
