9a57f7d20b27a41a60cb91ef069432f8568e8ac0
Emanuel Facundo <emanuel.facundo14@gmail.com>
2025-03-20 17:31:14 -0300

    fix: define valor padrão para href no componente item do Breadcrumb

--- app/components/ink_components/breadcrumb/list/item/component.rb
+++ app/components/ink_components/breadcrumb/list/item/component.rb
@@ -13,7 +13,7 @@ module InkComponents
 
           renders_one :next_page, lambda { InkComponents::Icon::Component.new(name: "chevron-right", type: :outline, class: "rtl:rotate-180 h-6 w-6 text-gray-400 mx-1") }
 
-          def initialize(href:, **extra_attributes)
+          def initialize(href: "#", **extra_attributes)
             @href = href
 
             super(**extra_attributes)
