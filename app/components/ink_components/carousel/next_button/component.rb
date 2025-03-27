6cb8dafc7f39502817ff13131ae78581b04e7d59
Emanuel Facundo <emanuel.facundo14@gmail.com>
2025-03-27 13:31:24 -0300

    feat: adiciona componentes NextButton e PrevButton para o Carousel

---
+++ app/components/ink_components/carousel/next_button/component.rb
@@ -0,0 +1,25 @@
+module InkComponents
+  module Carousel
+    module NextButton
+      class Component < ApplicationComponent
+        style do
+          base { %w[ absolute top-0 end-0 z-30 h-full !p-0 cursor-pointer group focus:outline-none inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none ] }
+        end
+
+        def initialize(**extra_attributes)
+          super(**extra_attributes)
+        end
+
+        private
+
+        def default_attributes
+          {
+            class: style,
+            data: { carousel_next: "" },
+            type: "button"
+          }
+        end
+      end
+    end
+  end
+end
